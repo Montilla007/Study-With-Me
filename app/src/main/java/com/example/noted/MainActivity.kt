e4eb50a04ec3168cee597985feee26ebff1a89a2
Montilla007 <rchristerdale@gmail.com>
2023-10-19 20:34:06 +0800

    Notepad done

---
+++ app/src/main/java/com/example/noted/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.noted
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.button)
+
+        btn.setOnClickListener{
+            val intent = Intent(this@MainActivity, NoteList::class.java)
+            startActivity(intent)
+        }
+    }
+}
